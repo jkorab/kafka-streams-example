38063dfb29fb25e93804f235d91186871ee45605
Jakub Korab <jakub.korab@gmail.com>
2016-08-15 20:46:59 +0100

    Initial commit

---
+++ src/main/java/com/ameliant/examples/KafkaStreamsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ameliant.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsExampleApplication.class, args);
+	}
+}
